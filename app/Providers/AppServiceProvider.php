37441c610c8290ceca7473225a8b806d2b36dead
NEO <39527922+skulldice003@users.noreply.github.com>
2026-02-09 04:22:56 +0700

    update_guild_war_formation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,14 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if ($this->app->environment('production') || str_contains(config('app.url'), 'https://')) {
             URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS', 'on');
         }
 
-        if ($this->app->environment('local')) {
-            URL::forceScheme('http');
-        }
+        // if ($this->app->environment('local')) {
+        //     URL::forceScheme('http');
+        // }
 
         Event::listen(SocialiteWasCalled::class, [DiscordExtendSocialite::class, 'handle']);
     }
