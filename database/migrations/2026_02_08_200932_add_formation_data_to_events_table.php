cbed820b547aeab9088ecbacf68bbb1a79438d87
NEO <39527922+skulldice003@users.noreply.github.com>
2026-02-09 03:10:17 +0700

    update_guild_war_formation

--- database/migrations/2026_02_08_200932_add_formation_data_to_events_table.php
+++ database/migrations/2026_02_08_200932_add_formation_data_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            //
+            $table->json('formation_data')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            //
+            $table->dropColumn('formation_data');
         });
     }
 };
