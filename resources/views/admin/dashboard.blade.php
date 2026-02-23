274606cf0fea706b6feef6311f6f18eb86236ef7
wwmcontrol6666 <wwmcontrol6666@gmail.com>
2026-02-23 13:33:56 +0700

    update dash board for staff

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,32 +5,63 @@
 @section('content')
 <div class="row">
     <div class="col-lg-3 col-6">
-        <!-- small box -->
         <div class="small-box bg-info">
             <div class="inner">
-                <h3>150</h3>
-                <p>New Orders</p>
+                <h3>{{ $membersCount }}</h3>
+                <p>Guild Members</p>
             </div>
             <div class="icon">
-                <i class="ion ion-bag"></i>
+                <i class="fas fa-users"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{ route('admin.users.index') }}" class="small-box-footer">
+                View members <i class="fas fa-arrow-circle-right"></i>
+            </a>
         </div>
     </div>
-    <!-- ./col -->
+
     <div class="col-lg-3 col-6">
-        <!-- small box -->
         <div class="small-box bg-success">
             <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
-                <p>Bounce Rate</p>
+                <h3>{{ $eventsRunning }}</h3>
+                <p>Running / Upcoming Events</p>
             </div>
             <div class="icon">
-                <i class="ion ion-stats-bars"></i>
+                <i class="fas fa-calendar-check"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{ route('admin.events.index') }}" class="small-box-footer">
+                View events <i class="fas fa-arrow-circle-right"></i>
+            </a>
         </div>
     </div>
-    <!-- ./col -->
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-secondary">
+            <div class="inner">
+                <h3>{{ $eventsCompleted }}</h3>
+                <p>Completed Events</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-flag-checkered"></i>
+            </div>
+            <a href="{{ route('admin.events.index') }}" class="small-box-footer">
+                View events <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>{{ $roles['Healer'] ?? 0 }} / {{ $roles['Tanker'] ?? 0 }} / {{ $roles['DPS'] ?? 0 }}</h3>
+                <p>Members by Role (Healer / Tanker / DPS)</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-user-shield"></i>
+            </div>
+            <a href="{{ route('admin.users.index') }}" class="small-box-footer">
+                View members <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
 </div>
 @endsection
