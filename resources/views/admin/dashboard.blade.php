17a18b1f1f64a87fdc49b7437c8faafcdf1d1fc5
NEO <39527922+skulldice003@users.noreply.github.com>
2026-02-23 19:53:20 +0700

    update text constant

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 
-@section('title', 'Dashboard')
+@section('title', __('messages.admin_dashboard_title'))
 
 @section('content')
 <div class="row">
@@ -8,13 +8,13 @@
         <div class="small-box bg-info">
             <div class="inner">
                 <h3>{{ $membersCount }}</h3>
-                <p>Guild Members</p>
+                <p>{{ __('messages.dashboard_guild_members') }}</p>
             </div>
             <div class="icon">
                 <i class="fas fa-users"></i>
             </div>
             <a href="{{ route('admin.users.index') }}" class="small-box-footer">
-                View members <i class="fas fa-arrow-circle-right"></i>
+                {{ __('messages.dashboard_view_members') }} <i class="fas fa-arrow-circle-right"></i>
             </a>
         </div>
     </div>
@@ -23,13 +23,13 @@
         <div class="small-box bg-success">
             <div class="inner">
                 <h3>{{ $eventsRunning }}</h3>
-                <p>Running / Upcoming Events</p>
+                <p>{{ __('messages.dashboard_running_events') }}</p>
             </div>
             <div class="icon">
                 <i class="fas fa-calendar-check"></i>
             </div>
             <a href="{{ route('admin.events.index') }}" class="small-box-footer">
-                View events <i class="fas fa-arrow-circle-right"></i>
+                {{ __('messages.dashboard_view_events') }} <i class="fas fa-arrow-circle-right"></i>
             </a>
         </div>
     </div>
@@ -38,13 +38,13 @@
         <div class="small-box bg-secondary">
             <div class="inner">
                 <h3>{{ $eventsCompleted }}</h3>
-                <p>Completed Events</p>
+                <p>{{ __('messages.dashboard_completed_events') }}</p>
             </div>
             <div class="icon">
                 <i class="fas fa-flag-checkered"></i>
             </div>
             <a href="{{ route('admin.events.index') }}" class="small-box-footer">
-                View events <i class="fas fa-arrow-circle-right"></i>
+                {{ __('messages.dashboard_view_events') }} <i class="fas fa-arrow-circle-right"></i>
             </a>
         </div>
     </div>
@@ -53,13 +53,13 @@
         <div class="small-box bg-warning">
             <div class="inner">
                 <h3>{{ $roles['Healer'] ?? 0 }} / {{ $roles['Tanker'] ?? 0 }} / {{ $roles['DPS'] ?? 0 }}</h3>
-                <p>Members by Role (Healer / Tanker / DPS)</p>
+                <p>{{ __('messages.dashboard_members_by_role') }}</p>
             </div>
             <div class="icon">
                 <i class="fas fa-user-shield"></i>
             </div>
             <a href="{{ route('admin.users.index') }}" class="small-box-footer">
-                View members <i class="fas fa-arrow-circle-right"></i>
+                {{ __('messages.dashboard_view_members') }} <i class="fas fa-arrow-circle-right"></i>
             </a>
         </div>
     </div>
