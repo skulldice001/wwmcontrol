51d35d4356a144862b0f712e2cc13ebd3e87a4f0
wwmcontrol6666 <wwmcontrol6666@gmail.com>
2026-02-27 17:15:51 +0700

    fix select skill

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,15 +8,12 @@ import 'datatables.net-bs4';
 import 'datatables.net-responsive';
 import 'datatables.net-responsive-bs4';
 
-// Import plugins dynamically to ensure jQuery is available globally
-import('select2/dist/js/select2.full.min.js').then((select2) => {
-    select2.default(jQuery);
-});
+// Import Select2 synchronously
+import select2 from 'select2/dist/js/select2.full.min.js';
+select2(window.jQuery);
 
-import('bootstrap/dist/js/bootstrap.bundle.min.js');
-
-// Import AdminLTE dynamically to ensure window.$ is set before it runs
-import('admin-lte/dist/js/adminlte.min.js');
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
 
 import { createApp } from 'vue';
 import CreateEventForm from './components/CreateEventForm.vue';
