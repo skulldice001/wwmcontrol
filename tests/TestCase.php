145c0fbe5af2e78dd85d17e6d3dd478a05e68907
NEO <39527922+skulldice003@users.noreply.github.com>
2026-03-05 19:35:23 +0700

    update test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Disable CSRF middleware for all tests
+        $this->withoutMiddleware(ValidateCsrfToken::class);
+    }
 }
